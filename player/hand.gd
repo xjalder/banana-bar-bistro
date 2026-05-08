0bb6587b1b640e75e6a3db73b66b381ec3ffb850
xjalder <xjalder@outlook.com>
2026-05-08 23:21:38 +1000

    Implemented player with arm movement and basic test area

---
+++ player/hand.gd
@@ -0,0 +1,22 @@
+class_name Hand extends RigidBody2D
+
+var grabbing : bool
+var arm_action : String
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if !grabbing:
+		var local_pos := (get_global_mouse_position() - global_position).normalized()
+		apply_central_impulse(local_pos * 100)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(arm_action):
+		var local_pos := (get_global_mouse_position() - global_position).normalized()
+		apply_central_impulse(local_pos * 1000)
+		grabbing = true
+	elif event.is_action_released(arm_action):
+		grabbing = false
+		if freeze:
+			SignalBus.ungrapple.emit()
+			freeze = false
+		
---
+++ signal_bus.gd
@@ -0,0 +1,4 @@
+extends Node
+
+signal grapple
+signal ungrapple
