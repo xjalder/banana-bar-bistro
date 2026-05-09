f5301d769372c296a7b8d22227558c4bca491bb1
xjalder <xjalder@outlook.com>
2026-05-09 17:42:58 +1000

    Food now can be picked up with double press

--- logic/input_handler.gd
+++ logic/input_handler.gd
@@ -1,7 +1,6 @@
 extends Node
 
 signal left_arm_interact_pressed
-
 signal right_arm_interact_pressed
 
 var q_timer : Timer
@@ -13,13 +12,15 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_arm"):
-		if q_timer.wait_time > 0:
+		if not q_timer.is_stopped():
 			left_arm_interact_pressed.emit()
 		else:
 			q_timer.start()
 	elif event.is_action_pressed("right_arm"):
-		if e_timer.wait_time > 0:
+		if not e_timer.is_stopped():
 			right_arm_interact_pressed.emit()
+		else:
+			e_timer.start()
 
 func setup_timer() -> Timer:
 	var timer := Timer.new()
