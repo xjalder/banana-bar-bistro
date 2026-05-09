e6777a45f61b11600ba5212e1b9a4b45a2036cc1
Massimo Spadaro <massimo.spadaro.au@gmail.com>
2026-05-09 18:06:48 +1000

    feat: Integrate shop item purchasing in GameManager

--- logic/game_manager.gd
+++ logic/game_manager.gd
@@ -5,37 +5,38 @@ var money: int
 
 var is_paused : bool = false
 
-var items = {"SEATS":100, "MAGIC":50, "DECO":120}
+var items = {Enums.Items.DECOS : 100, Enums.Items.CHAIRS:50, Enums.Items.MUSIC:120}
+var increases = {Enums.Items.DECOS : 2, Enums.Items.CHAIRS: 5, Enums.Items.MUSIC: 3}
 
 var upgrades = {
-	"capacity": 3,
-	"time_to_unhappy": 3,
-	"time_to_new_customer" : 4}
-
-var owned_items = []
+	Enums.Items.DECOS: 3,
+	Enums.Items.MUSIC: 3,
+	Enums.Items.CHAIRS : 4}
 
 signal show_cash(money : int)
 
-
+signal buy_order(_buy_item)
 
 func _add_money(income : int)->void:
 	money += income
 	show_cash.emit(money)
 
-func _buy_item(money : int, item: String)->void:
+func buy_item(item: Enums.Items)->bool:
 	if items.get(item) <= money:
+		print("BOUGHT! ", money)
 		money -= items.get(item)
-		owned_items.append(item)
+		for i in upgrades.keys(): #change item bought
+			if i == item:
+				upgrades[i] += increases.get(item)
+		return true
+	print("FAILED!", money)
+	return false
 		
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	money = 0
-	#BananaHut.connect(_add_money)
+	money = 500
 	
-	
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
