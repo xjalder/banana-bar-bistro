79f541106fd4796129dd8fe1548062785d8229ce
Massimo Spadaro <massimo.spadaro.au@gmail.com>
2026-05-09 18:06:47 +1000

    refactor: Expand Enums for shop system

--- logic/enums.gd
+++ logic/enums.gd
@@ -5,3 +5,11 @@ enum ArmType {Left, Right}
 enum Growables {None, Banana, Bread, Ice, Milk}
 
 enum Order {BANANA, BANANA_BREAD, BANANA_SMOOTHIE}
+
+enum Holdables {BANANA, BANANA_BREAD, BANANA_SMOOTHIE, BREAD, ICE, MILK, NONE}
+
+var OrderCost :Dictionary[Order, int] = {
+	Order.BANANA : 50, Order.BANANA_BREAD : 90, Order.BANANA_SMOOTHIE : 120
+	}
+	
+enum Items {DECOS, CHAIRS, MUSIC}
