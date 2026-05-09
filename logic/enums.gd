b7e7eb428afd2bdf15b6505020e74f5e2517fb7e
Wugene1 <bencuffe1@gmail.com>
2026-05-09 17:43:04 +1000

    added icecream

--- logic/enums.gd
+++ logic/enums.gd
@@ -4,9 +4,9 @@ enum ArmType {Left, Right}
 
 enum Growables {None, Banana, Bread, Ice, Milk}
 
-enum Order {BANANA, BANANA_BREAD, BANANA_SMOOTHIE}
+enum Order {BANANA, BANANA_BREAD, BANANA_SMOOTHIE, BANANA_ICECREAM}
 
-enum Holdables {BANANA, BANANA_BREAD, BANANA_SMOOTHIE, BREAD, ICE, MILK, NONE}
+enum Holdables {BANANA, BANANA_BREAD, BANANA_SMOOTHIE, BANANA_ICECREAM, BREAD, ICE, MILK, NONE}
 
 var OrderCost :Dictionary[Order, int] = {
 	Order.BANANA : 50, Order.BANANA_BREAD : 90, Order.BANANA_SMOOTHIE : 120
