ee99d176bc858fe771663111756b7f9ae9470ed6
Massimo Spadaro <massimo.spadaro.au@gmail.com>
2026-05-09 18:44:59 +1000

    Refactor: Temporarily disable exit button ready connection

--- ui/exit.gd
+++ ui/exit.gd
@@ -3,8 +3,8 @@ extends Button
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	self.pressed.connect()
+#func _ready() -> void:
+	#self.pressed.connect()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
